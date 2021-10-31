9d25ed1941e3b53e5569c9d9e1e97ccbcf8b96fa
Nikolay <67314479+PotapovNikolay@users.noreply.github.com>
2021-10-31 13:09:38 +0300

    lab2_5_1

--- lab2/lab2_5_1.py
+++ lab2/lab2_5_1.py
@@ -11,24 +11,19 @@ print("y",len(y[0]))
 h,w = image.shape[:2]
 print(image[0][0])
 
-image_sobol = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-sobelx = cv.Sobel(image_sobol,cv.CV_64F,1,0,ksize=5)
-sobely = cv.Sobel(image_sobol,cv.CV_64F,0,1,ksize=5)
-sobel = cv.Sobel(image_sobol,cv.CV_64F,1,1,ksize=5)
+plt.subplot(121), plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB)), plt.title('исходное изображение'), plt.axis('off')
 
-plt.subplot(2,2,1),plt.imshow(image_sobol,cmap = 'gray')
-plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,2),plt.imshow(sobel,cmap = 'gray')
-plt.title('Sobel '), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
-plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
-plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+image=cv.cvtColor(image, cv.COLOR_BGR2RGB)
+image=cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+x = cv.Sobel (image, cv.CV_16S, 1, 0)
+y = cv.Sobel (image, cv.CV_16S, 0, 1)
+absX = cv.convertScaleAbs(x)
+absY = cv.convertScaleAbs(y)
+Sobel = cv.addWeighted(absX, 0.5, absY, 0.5, 0)
 
+plt.subplot(122), plt.imshow(Sobel, cmap=plt.cm.gray), plt.title('оператор Собеля'), plt.axis('off')
 
 plt.show()
-
-image=cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 
 core_sobol_y=[[-1,0,1],
@@ -71,7 +66,7 @@ def pp(list_h, list_w):
                         sum0_y += round(image[list_h[i]][list_w[j]] * core_sobol_y[k][l])
 
     sum0 = math.sqrt((sum0_x ** 2) + (sum0_y ** 2))
-
+    # sum0 = sum0_x+sum0_y
     if sum0>255:
         sum0=255
     list_sum0.append(sum0)
