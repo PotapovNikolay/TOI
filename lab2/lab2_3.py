af20403f2dfee27c6ceca7386f72a0bd7a77c422
Nikolay <67314479+PotapovNikolay@users.noreply.github.com>
2021-11-01 23:31:37 +0300

    lab2_3

--- lab2/lab2_3.py
+++ lab2/lab2_3.py
@@ -33,10 +33,8 @@ image_peresvet = cv.imread('pic/peresvet3.jpg')
 image_dark = cv.imread('pic/dark.jpg')
 image_low_k = cv.imread('pic/low_k.jpg')
 
-cv.imshow("orig_before", image_orig)
-cv.imshow("peresvet_before", image_peresvet)
-cv.imshow("dark_before", image_dark)
-cv.imshow("low_k_before", image_low_k)
+cv.imshow("pic's before", np.concatenate((image_orig,image_peresvet,image_dark,image_low_k), axis=1))
+h, w = image_low_k.shape[:2]
 
 h, w = image_low_k.shape[:2]
 
@@ -54,23 +52,23 @@ list_of_name_of_image_before.extend(['hist_orig_YUV_before','hist_peresvet_YUV_b
 list_of_plot_of_image.extend([hist(image_orig_YUV),hist(image_peresvet_YUV),hist(image_dark_YUV),hist(image_low_k_YUV)])
 list_of_image.extend([image_orig_YUV,image_peresvet_YUV,image_dark_YUV,image_low_k_YUV])
 
-# for i in range(len(list_of_name_of_image_before)):
-#     plt.figure(i+1)
-#     plt.title(list_of_name_of_image_before[i])
-#     plt.plot(list_of_plot_of_image[i], '#FF7382')
-
 for i in range(len(list_of_name_of_image_before)):
-    hist = np.bincount(list_of_image[i].ravel(), minlength=256).tolist()
-    ll = DCF(hist)
-    ll = np.round(np.float32(ll) / (list_of_image[i].shape[0] * list_of_image[i].shape[1]), 4)
-    plt.figure(list_of_name_of_image_before[i]+"DCF", facecolor="lightgray")
-    plt.title("DCF")
-    plt.xlabel("index", fontsize=14)
-    plt.ylabel("value", fontsize=14)
-    plt.tick_params(labelsize=10)
-    plt.grid(linestyle=':')
-    plt.plot(range(0, 256), ll, c='dodgerblue', label=r'$P(rk)=n1+n2+n3+...Nk $')
-    plt.legend()
+    plt.figure(i+1)
+    plt.title(list_of_name_of_image_before[i])
+    plt.plot(list_of_plot_of_image[i], '#FF7382')
+
+# for i in range(len(list_of_name_of_image_before)):
+#     hist = np.bincount(list_of_image[i].ravel(), minlength=256).tolist()
+#     ll = DCF(hist)
+#     ll = np.round(np.float32(ll) / (list_of_image[i].shape[0] * list_of_image[i].shape[1]), 4)
+#     plt.figure(list_of_name_of_image_before[i]+"DCF", facecolor="lightgray")
+#     plt.title("DCF")
+#     plt.xlabel("index", fontsize=14)
+#     plt.ylabel("value", fontsize=14)
+#     plt.tick_params(labelsize=10)
+#     plt.grid(linestyle=':')
+#     plt.plot(range(0, 256), ll, c='dodgerblue', label=r'$P(rk)=n1+n2+n3+...Nk $')
+#     plt.legend()
 
 for i in range(h):
     for j in range(w):
@@ -80,20 +78,23 @@ for i in range(h):
         image_low_k_YUV[i][j][0] = ((image_low_k_YUV[i][j][0]/255) ** 2)*255
 
 
-cv.imshow("orig_after",cv.cvtColor(image_orig_YUV, cv.COLOR_YUV2BGR))
-cv.imshow("peresvet_after",cv.cvtColor(image_peresvet_YUV, cv.COLOR_YUV2BGR))
-cv.imshow("dark_after",cv.cvtColor(image_dark_YUV, cv.COLOR_YUV2BGR))
-cv.imshow("low_k_after",cv.cvtColor(image_low_k_YUV, cv.COLOR_YUV2BGR))
+# cv.imshow("orig_after",cv.cvtColor(image_orig_YUV, cv.COLOR_YUV2BGR))
+# cv.imshow("peresvet_after",cv.cvtColor(image_peresvet_YUV, cv.COLOR_YUV2BGR))
+# cv.imshow("dark_after",cv.cvtColor(image_dark_YUV, cv.COLOR_YUV2BGR))
+# cv.imshow("low_k_after",cv.cvtColor(image_low_k_YUV, cv.COLOR_YUV2BGR))
+cv.imshow("pic's_after",np.concatenate((cv.cvtColor(image_orig_YUV, cv.COLOR_YUV2BGR),cv.cvtColor(image_peresvet_YUV, cv.COLOR_YUV2BGR),
+                                       cv.cvtColor(image_dark_YUV, cv.COLOR_YUV2BGR), cv.cvtColor(image_low_k_YUV, cv.COLOR_YUV2BGR)), axis=1))
+
 
 list_of_name_of_image_after.extend(['hist_orig_YUV_after','hist_peresvet_YUV_after','hist_dark_YUV_after','hist_low_k_YUV_after'])
 list_of_plot_of_image_after.extend([hist2(image_orig_YUV),hist2(image_peresvet_YUV),hist2(image_dark_YUV),hist2(image_low_k_YUV)])
 list_of_image.extend([image_orig_YUV,image_peresvet_YUV,image_dark_YUV,image_low_k_YUV])
 
 
-# for i in range(len(list_of_name_of_image_after)):
-#     plt.figure(i+6)
-#     plt.title(list_of_name_of_image_after[i])
-#     plt.plot(list_of_plot_of_image_after[i], '#75B1FF')
+for i in range(len(list_of_name_of_image_after)):
+    plt.figure(i+6)
+    plt.title(list_of_name_of_image_after[i])
+    plt.plot(list_of_plot_of_image_after[i], '#75B1FF')
 
 # for i in range(len(list_of_name_of_image_after)):
 #     hist = np.bincount(list_of_image[i].ravel(), minlength=256).tolist()
