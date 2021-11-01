63817690d5feec32c16b02174499694918f259a6
Nikolay <67314479+PotapovNikolay@users.noreply.github.com>
2021-11-01 23:32:19 +0300

    lab2_5_3

--- lab2/lab2_5_3.py
+++ lab2/lab2_5_3.py
@@ -11,17 +11,9 @@ print("y",len(y[0]))
 h,w = image.shape[:2]
 print(image[0][0])
 
-plt.subplot(121), plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB)), plt.title('исходное изображение'), plt.axis('off')
-
 image=cv.cvtColor(image, cv.COLOR_BGR2RGB)
 image=cv.cvtColor(image, cv.COLOR_RGB2GRAY)
-x = cv.Sobel (image, cv.CV_16S, 1, 0)
-y = cv.Sobel (image, cv.CV_16S, 0, 1)
-absX = cv.convertScaleAbs(x)
-absY = cv.convertScaleAbs(y)
-Sobel = cv.addWeighted(absX, 0.5, absY, 0.5, 0)
 
-plt.subplot(122), plt.imshow(Sobel, cmap=plt.cm.gray), plt.title('оператор Собеля'), plt.axis('off')
 
 plt.show()
 
@@ -32,7 +24,7 @@ core_sobol_x=[[-1,0,1],
 
 core_sobol_y=[[-1,-1,-1],
               [0,0,0],
-              [1,2+1,1]]
+              [1,1,1]]
 
 flag=True
 
@@ -57,8 +49,8 @@ def pp(list_h, list_w):
                         sum0_x += round(image[list_h[i]][list_w[j]] * core_sobol_x[k][l])
                         sum0_y += round(image[list_h[i]][list_w[j]] * core_sobol_y[k][l])
 
-    #sum0 = math.sqrt((sum0_x ** 2) + (sum0_y ** 2))
-    sum0 = sum0_x+sum0_y
+    sum0 = math.sqrt((sum0_x ** 2) + (sum0_y ** 2))
+    #sum0 = sum0_x+sum0_y
     if sum0>255:
         sum0=255
     list_sum0.append(sum0)
@@ -94,8 +86,5 @@ black=black.astype('float32')
 
 cv.imshow("after hand", cv.cvtColor(black, cv.COLOR_GRAY2BGR))
 cv.imwrite('pic/bb3.png',cv.cvtColor(black, cv.COLOR_GRAY2BGR))
-
-
-
 
 cv.waitKey(0)
